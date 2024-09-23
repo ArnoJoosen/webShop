cfdb9c527c6701909a267e20a7fe104fde792174
Arno <arno@arnoPC.localdomain>
2024-09-23 16:37:46 +0200

    start

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <?php
+    echo "<p>Hello World</p>";
+    echo "test 1";
+    echo "test 2";
+    echo "test 3";
+    echo "test 4";
+    ?>
+</body>
+</html>
+            
